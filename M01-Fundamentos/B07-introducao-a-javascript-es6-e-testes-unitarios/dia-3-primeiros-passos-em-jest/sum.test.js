3cdbdd95f52b12627a6898161fb121eeb90746f7
Danilo do Valle <danilo62d@gmail.com>
2021-11-12 19:23:29 -0300

    Requisito 1

---
+++ M01-Fundamentos/B07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -0,0 +1,23 @@
+// 1 - A função sum(a, b) retorna a soma do parâmetro a com o b
+//   1 - Teste se o retorno de sum(4, 5) é 9
+//   2 - Teste se o retorno de sum(0, 0) é 0
+//   3 - Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+//   4 - Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+
+const sum = require("./sum");
+
+test('1 - Testa se o retorno de sum(4, 5) é 9', () => {
+  expect(sum(4, 5)).toEqual(9);
+});
+
+test('2 - Testa se o retorno de sum(0, 0) é 0', () => {
+  expect(sum(0, 0)).toEqual(0);
+});
+
+test('3 - Testa se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)', () => {
+  expect(() => sum(4, "5")).toThrow(Error);
+});
+
+test('4 - Testa se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+  expect(() => sum(4, "5")).toThrow('parameters must be numbers');
+});
