58beb1571b7d2badd8a3aa76e24389a03ddde71f
Danilo do Valle <danilo62d@gmail.com>
2021-11-22 20:00:22 -0300

    exercise 3

--- M01-Fundamentos/B08-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise3.js
+++ M01-Fundamentos/B08-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise3.js
@@ -1,7 +1,29 @@
+const { books } = require('./dados');
+const assert = require('assert');
+
 // 3 - Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
 
 const expectedResult = 43;
 
+// Opção com função:
+
 function averageAge() {
-  // escreva seu código aqui
+  const result = books.reduce(
+    (acc, curr) => acc + (curr.releaseYear - curr.author.birthYear),
+    0,
+  );
+  return result / books.length;
 }
+
+console.log(averageAge());
+assert.deepStrictEqual(averageAge(), expectedResult);
+
+// Opção sem função:
+
+const result = books.reduce(
+  (acc, curr) => acc + (curr.releaseYear - curr.author.birthYear),
+  0,
+);
+
+console.log(result / books.length);
+assert.deepStrictEqual(result / books.length, expectedResult);
